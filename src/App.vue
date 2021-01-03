0ab6f923f0053b285dae85053d2dd8b47aeed34a
Francisco Vena <fvena32@gmail.com>
2021-01-03 23:24:11 +0100

    build(template): añadido soporte para Pug

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<template lang="pug">
+img(alt="Vue logo" src="./assets/logo.png")
+HelloWorld(msg="Hello Vue 3 + Vite + Pug")
 </template>
 
 <script setup>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,13 @@
-<template>
-  <h1>{{ msg }}</h1>
+<template lang="pug">
+h1 {{ msg }}
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+p
+  a(href="https://vitejs.dev/guide/features.html" target="_blank") Vite Documentation |
+  a(href="https://v3.vuejs.org/" target="_blank") Vue 3 Documentation
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+button(@click="state.count++") count is: {{ state.count }}
+
+p Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
 </template>
 
 <script setup>
