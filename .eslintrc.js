facaccec92385409f1e57dc415638a81ea736364
Francisco Vena <fvena32@gmail.com>
2021-01-06 20:59:20 +0100

    build(linters): actualizada la configuración de los linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true,
   },
-  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "prettier", "prettier/vue"],
-  plugins: ["vue"],
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier', 'prettier/vue'],
+  plugins: ['vue'],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       alias: {
         map: [
-          ["Core", "./src/modules/core"],
-          ["Auth", "./src/modules/auth"],
+          ['Core', './src/modules/core'],
+          ['Auth', './src/modules/auth'],
         ],
       },
     },
   },
   rules: {
-    "vue/no-unused-vars": "off",
-    "no-unused-vars": "off",
+    'vue/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
     'vue/max-attributes-per-line': ['warn', { singleline: 5 }],
   },
 };
